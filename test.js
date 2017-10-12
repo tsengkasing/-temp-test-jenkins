4d72218a64540ca106fbd76b97161a595daac683
tsengkasing <tsengkasing@gmail.com>
2017-10-12 22:18:38 +0800

    ......

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
  * Created by tsengkasing on 4/25/2017.
  */
 
-const GPA = require('./GPA');
-console.log(123);
-GPA({token1: 'abc', token2: 'abc'}, (obj) => {console.log(obj);});
+const GPA = require('./GPA')
+console.log(123)
+GPA({ token1: 'abc', token2: 'abc' }, (obj) => { 
+	console.log(obj); 
+})
